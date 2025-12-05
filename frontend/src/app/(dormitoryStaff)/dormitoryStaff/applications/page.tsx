94dbbd64ce14293a317791b9e04d896b4add8159
Taraumaru <ctara9582@gmail.com>
2025-12-05 10:11:00 +0900

    add forwardlink component

--- frontend/src/app/(dormitoryStaff)/dormitoryStaff/applications/page.tsx
+++ frontend/src/app/(dormitoryStaff)/dormitoryStaff/applications/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function DormitoryStaffApplications() {
     return(
         <div>
             <h1>DormitoryStaffApplications</h1>
+            <Link href="/dormitoryStaff/dashboard">ダッシュボード</Link>
         </div>
     )
 }
